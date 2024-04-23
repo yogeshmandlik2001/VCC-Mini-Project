8ee099d00506670e86af229020abaa90ee6f0224
yogeshmandlik2001 <68507246+yogeshmandlik2001@users.noreply.github.com>
2024-04-23 20:22:11 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask
+import socket
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    container_id = socket.gethostname()
+    return f"Hello from Flask! (Container ID: {container_id})"
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
