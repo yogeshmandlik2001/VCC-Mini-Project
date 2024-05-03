c8780f5c0fe7d3db667fb51548a69e442e6058dd
yogeshmandlik2001 <68507246+yogeshmandlik2001@users.noreply.github.com>
2024-05-03 14:17:26 +0530

    Updated app.py

--- app.py
+++ app.py
@@ -9,4 +9,4 @@ def hello():
     return f"Hello from Flask! (Container ID: {container_id})"
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=True, host='0.0.0.0', port=4001)
