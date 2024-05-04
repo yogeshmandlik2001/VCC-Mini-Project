ec5ac561ff7ecfb7f99a1e17354dd1429a852786
Vikrant Singh <72123544+Vikrant997@users.noreply.github.com>
2024-05-04 05:36:14 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
     container_id = socket.gethostname()
-    return f"Hello from Flask! (Container ID: {container_id})"
+    return f"Hello from Flask!! (This is my Container ID: {container_id}!! ) BYE BYE!!"
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=4001)
